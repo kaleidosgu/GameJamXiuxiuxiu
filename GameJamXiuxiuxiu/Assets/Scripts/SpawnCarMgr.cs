4879d21112c2d2bd07cd6c2a933f46918906c3a8
kaleidos <gjcaoyi@163.com>
2019-12-20 17:20:40 +0800

    场景和生成接口添加

--- GameJamXiuxiuxiu/Assets/Scripts/SpawnCarMgr.cs
+++ GameJamXiuxiuxiu/Assets/Scripts/SpawnCarMgr.cs
@@ -7,18 +7,25 @@ public class SpawnCarMgr : MonoBehaviour
     public GameObject PrefabCar;
 
     public SpawnPosition[] ArraySpawnPos;
+	
+	public SpawnPosition NorthPos;
+	public SpawnPosition SouthPos;
+	public SpawnPosition EastPos;
+	public SpawnPosition WestPos;
 
     public float SpawnCarTime;
 
     public ChangeSpeedMgr chgSpeedMgr;
 
     public bool DebugTest;
+	public GlobalDefine.CarMovementDir MoveDir;
     private float m_fCurSpawnCarTime;
     private int m_nCntLst;
     // Start is called before the first frame update
     void Start()
     {
         m_nCntLst = ArraySpawnPos.Length;
+		
     }
 
     // Update is called once per frame
@@ -27,15 +34,40 @@ public class SpawnCarMgr : MonoBehaviour
         if (DebugTest == true)
         {
             DebugTest = false;
-            _generate();
+            //_randomSpawn();
+			
+			GenerateByDir(MoveDir,PrefabCar);
         }
     }
-
-    private void _generate()
-    {
+	public void GenerateByDir(GlobalDefine.CarMovementDir _dir,GameObject _obj)
+	{
+		if( _dir == GlobalDefine.CarMovementDir.CarMovementDir_North )
+		{
+			_generate(NorthPos,PrefabCar);
+		}
+		else if( _dir == GlobalDefine.CarMovementDir.CarMovementDir_South )
+		{
+			_generate(SouthPos,PrefabCar);
+		}
+		else if( _dir == GlobalDefine.CarMovementDir.CarMovementDir_East )
+		{
+			_generate(EastPos,PrefabCar);
+		}
+		else if( _dir == GlobalDefine.CarMovementDir.CarMovementDir_West )
+		{
+			_generate(WestPos,PrefabCar);
+		}
+	}
+		
+	private void _randomSpawn()
+	{
         int nRandom = Random.Range(0, m_nCntLst);
         SpawnPosition _pos = ArraySpawnPos[nRandom];
-        GameObject _objCar = Instantiate(PrefabCar, Vector3.zero, Quaternion.identity);
+		_generate(_pos,PrefabCar);
+	}
+    private void _generate(SpawnPosition _pos,GameObject _objPrefab)
+    {
+        GameObject _objCar = Instantiate(_objPrefab, Vector3.zero, Quaternion.identity);
         _objCar.transform.SetParent(_pos.transform);
         _objCar.transform.localPosition = Vector3.zero;
         CarMovement _carMovement = _objCar.GetComponent<CarMovement>();
@@ -49,7 +81,7 @@ public class SpawnCarMgr : MonoBehaviour
         if(m_fCurSpawnCarTime >= SpawnCarTime)
         {
             m_fCurSpawnCarTime -= SpawnCarTime;
-            _generate();
+            _randomSpawn();
         }
     }
 }
