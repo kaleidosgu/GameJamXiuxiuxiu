79eb2d3b36a2e848ca50049f912c0249f6a4ea5f
kaleidos <gjcaoyi@163.com>
2019-12-20 11:22:45 +0800

    东西朝向不管加速 东西只有一条会产生汽车

--- GameJamXiuxiuxiu/Assets/Scripts/RequestCommandMgr.cs
+++ GameJamXiuxiuxiu/Assets/Scripts/RequestCommandMgr.cs
@@ -9,6 +9,7 @@ public class RequestCommandMgr : MonoBehaviour
     public SoundSpeaker SndSpeaker;
     public ChangeSpeedMgr ChangeSpeedMgr;
     public int CountsOfLst;
+    public bool ExecuteWestAndEast;
 
     private float m_fCurrentTimeToExecute;
     private List<RequestCommand> m_lstCommand;
@@ -41,7 +42,17 @@ public class RequestCommandMgr : MonoBehaviour
             else if (m_lstCommand[0].GetCmdTyp() == RequestCommand.RequestCommand_Type.RequestCommand_Type_ChangeSpeed)
             {
                 RequestChangeSpeed _changeSpeed = (RequestChangeSpeed)m_lstCommand[0];
-                ChangeSpeedMgr.ExecuteSpeed(_changeSpeed.GetCarDir());
+                if(_changeSpeed.GetCarDir() == GlobalDefine.CarMovementDir.CarMovementDir_East || _changeSpeed.GetCarDir() == GlobalDefine.CarMovementDir.CarMovementDir_West)
+                {
+                    if( ExecuteWestAndEast == true )
+                    {
+                        ChangeSpeedMgr.ExecuteSpeed(_changeSpeed.GetCarDir());
+                    }
+                }
+                else
+                {
+                    ChangeSpeedMgr.ExecuteSpeed(_changeSpeed.GetCarDir());
+                }
             }
             else
             {
